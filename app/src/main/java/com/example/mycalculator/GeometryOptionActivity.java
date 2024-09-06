187d16b746fe50a1e786dc5f8599096c02fa08c0
cutejaeyoon <sohwa.s200352@gge.goe.go.kr>
2024-09-06 19:37:58 +0900

    20240906

--- app/src/main/java/com/example/mycalculator/GeometryOptionActivity.java
+++ app/src/main/java/com/example/mycalculator/GeometryOptionActivity.java
@@ -3,11 +3,24 @@ package com.example.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class GeometryOptionActivity extends AppCompatActivity {
-
+Button btnCircle;
+Button btnTrapezoid;
+Button btnParallelogram;
+Button btnTriangle;
+Button btnRhombus;
+ImageView imgMollu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        btnCircle=findViewById(R.id.btnCircle);
+        btnTrapezoid=findViewById(R.id.btnTrapezoid);
+        btnParallelogram=findViewById(R.id.btnParallelogram);
+        btnTriangle=findViewById(R.id.btnTriangle);
+        btnRhombus=findViewById(R.id.btnRhombus);
+        imgMollu=findViewById(R.id.imageView2);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geometry_option);
     }
